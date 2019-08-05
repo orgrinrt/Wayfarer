c235b3b0c8fa7f1e9d8f15add4f5d68a3e9ee014
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-05 15:21:34 +0300

    Added in Inspector enhancements, beginning with the InspectorLabel (due to be renamed in future)

--- ModuleSystem/module_meta.gd
+++ ModuleSystem/module_meta.gd
@@ -6,4 +6,3 @@ export var desc : String;
 export var version : String;
 export var deps : Array; # array of strings with dependency modules' name only
 export var deps_vers : Array; # array of strings with dependency modules' version only
-var ref : EditorPlugin;
