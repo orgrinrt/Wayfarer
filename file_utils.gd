5fe472dd6b140072c4b3a976644586ce30e2b2e1
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-07 12:58:41 +0300

    -Added in some checks to see for inregular states, such as getting a null filesystem etc and pushing errors where applicable -Added in expect_empties argument to the file_util methods for debugging purposes (i.e pushing errors when getting files from dir/gettting subdirs resulted in an empty result)

--- file_utils.gd
+++ file_utils.gd
@@ -7,17 +7,31 @@ const FILTER_PEBBLER = ["*.pebbler"]
 const FILTER_RES = ["*.tres"]
 const FILTER_SCENE = ["*.tscn"]
 
-var plugin : EditorPlugin;
-var filesystem : EditorFileSystem;
+var plugin : EditorPlugin = null;
+var filesystem : EditorFileSystem = null;
 
-func get_files_from_dir(path, return_path = true, filter = FILTER_DEFAULT) -> Array:
+func get_files_from_dir(path, return_path = true, filter = FILTER_DEFAULT, expect_empties = true) -> Array:
 	var files : Array = [];
 	
+	if !is_instance_valid(filesystem):
+		push_error("filesystem was null");
+		return files
+		
 	var dir = filesystem.get_filesystem_path(path)
 	if !is_instance_valid(dir):
-		return files
+		if (dir == null):
+			push_error("filesystem.get_filesystem_path returned an invalid instance");
+			return files
+		else:
+			Log.Wf.Print("...but it was not null so it probably is valid", true);
 
-	for i in dir.get_file_count():
+	var file_count = dir.get_file_count();
+	
+	if !expect_empties:
+		if file_count < 1:
+			push_error("dir.get_file_count() was less than 1");
+	
+	for i in file_count:
 		var file = dir.get_file(i)
 
 		if _match_name(file, filter):
@@ -27,14 +41,28 @@ func get_files_from_dir(path, return_path = true, filter = FILTER_DEFAULT) -> Ar
 				files.append(file)
 	return files
 
-func get_dirs(path, return_path = true, filter = FILTER_DEFAULT):
+func get_dirs(path, return_path = true, filter = FILTER_DEFAULT, expect_empties = true):
 	var dirs : Array = [];
+	
+	if !is_instance_valid(filesystem):
+		push_error("filesystem was null");
+		return dirs
 	
 	var dir = filesystem.get_filesystem_path(path)
 	if !is_instance_valid(dir):
-		return dirs
+		if (dir == null):
+			push_error("filesystem.get_filesystem_path returned an invalid instance");
+			return dirs
+		else:
+			Log.Wf.Print("...but it was not null so it probably is valid", true);
 
-	for i in dir.get_subdir_count():
+	var subdir_count = dir.get_subdir_count();
+	
+	if !expect_empties:
+		if subdir_count < 1:
+			push_error("dir.get_subdir_count() was less than 1");
+	
+	for i in subdir_count:
 		var subdir = dir.get_subdir(i)
 		var name = subdir.get_name()
 
@@ -48,10 +76,10 @@ func get_dirs(path, return_path = true, filter = FILTER_DEFAULT):
 func get_files_recursive(path, filter = FILTER_DEFAULT) -> Array:
 	var files : Array = [];
 	
-	for file_in_root in get_files_from_dir(path, true, filter):
+	for file_in_root in get_files_from_dir(path, true, filter, true):
 			files.append(file_in_root);
 
-	for sub_dir in get_dirs(path):
+	for sub_dir in get_dirs(path, true, FILTER_DEFAULT, true):
 		var sub_files = get_files_recursive(sub_dir, filter);
 		for sub_file in sub_files:
 			files.append(sub_file);
@@ -70,3 +98,6 @@ func _match_name(name : String, filter) -> bool:
 func set_plugin(var p : EditorPlugin):
 	plugin = p;
 	filesystem = plugin.get_editor_interface().get_resource_filesystem()
+	
+	if (filesystem == null || !is_instance_valid(filesystem)):
+		push_error("filesystem was null even after setting plugin!");
