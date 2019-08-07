180b19f82901ad58365fe09049a034e0c5530a4a
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-07 12:51:57 +0300

    Separated modules' _EnterTree, _Ready and _ExitTree methods to safe alternatives so that we can ensure they do not fire their logic when the moduleSystem doesn't want them to

--- ModuleSystem/WayfarerModule.cs
+++ ModuleSystem/WayfarerModule.cs
@@ -1,12 +1,10 @@
 #if TOOLS
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.CompilerServices;
 using Godot;
 using Godot.Collections;
 using Wayfarer.Utils.Debug;
-using Wayfarer.Utils.Files;
 using Array = Godot.Collections.Array;
 using Object = Godot.Object;
 
@@ -21,25 +19,69 @@ namespace Wayfarer.ModuleSystem
         public Array ModuleDependencies => GetModuleDependencies();
         public Array ModuleDependencyVersions => GetModuleDependencyVersions();
         public ModuleMeta ModuleMeta => GetModuleMeta();
-        public WayfarerProjectSettings WayfarerProjectSettings => GetWayfarerSettings();
+        public bool? ResetOnReady => WayfarerProjectSettings.ResetOnReady;
+
+        private bool _cachedResetOnReady = true;
 
         public override void _EnterTree()
         {
             base._EnterTree();
-            EnablePlugin();
+            
+            if (ResetOnReady != null)
+            {
+                _cachedResetOnReady = (bool)ResetOnReady;
+                
+                if ((bool)ResetOnReady) return;
+                
+                Log.Wf.Print("ResetOnReady was false - this must be the OW reset after launch!", true);
+                
+                EnablePlugin();
+                _EnterTreeSafe();
+            }
+            else
+            {
+                throw new NullReferenceException("ResetOnReady was null");
+            }
         }
 
         public override void _Ready()
         {
             base._Ready();
             
-            Name = ModuleName;
+            if (_cachedResetOnReady) return;
+            
+            try
+            {
+                Name = ModuleName;
+            }
+            catch (Exception e)
+            {
+                Log.Wf.Error("Couldn't set the plugin's (" + Name + ") name to ModuleName", e, true);
+            }
+
+            _ReadySafe();
         }
 
         public override void _ExitTree()
         {
             base._ExitTree();
+            _ExitTreeSafe();
             DisablePlugin();
+        }
+
+        public virtual void _EnterTreeSafe()
+        {
+            
+        }
+
+        public virtual void _ReadySafe()
+        {
+            
+        }
+
+        public virtual void _ExitTreeSafe()
+        {
+            
         }
 
         private Dictionary GetWayfarerSettingsDictionary()
@@ -53,11 +95,6 @@ namespace Wayfarer.ModuleSystem
             Dictionary settings = (Dictionary) settingRes.Get("settings");
 
             return settings;
-        }
-
-        private WayfarerProjectSettings GetWayfarerSettings()
-        {
-            return new WayfarerProjectSettings();
         }
 
         private ModuleMeta GetModuleMeta()
@@ -88,118 +125,6 @@ namespace Wayfarer.ModuleSystem
         private Array GetModuleDependencyVersions()
         {
             return ModuleMeta.DepsVersions;
-        }
-    }
-
-    public struct ModuleMeta
-    {
-        private Resource _resource;
-        public Resource Resource => _resource;
-
-        private string _name;
-        private string _desc;
-        private string _version;
-        private Array _deps;
-        private Array _depsVersions;
-
-        public string Name => _name;
-        public string Desc => _desc;
-        public string Version => _version;
-        public Array Deps => _deps;
-        public Array DepsVersions => _depsVersions;
-
-        public ModuleMeta(WayfarerModule reference)
-        {
-            string path = (string) reference.GetScript().Get("resource_path");
-            string[] split = path.Split("/");
-            List<string> pathParts = split.ToList();
-            pathParts.RemoveAt(pathParts.Count - 1);
-            string parentPath = "";
-
-            foreach (string s in pathParts)
-            {
-                if (s.StartsWith("res:"))
-                {
-                    parentPath = s + "//";
-                    continue;
-                }
-
-                if (s == "addons")
-                {
-                    parentPath = parentPath + "Addons" + "/";
-                    continue;
-                }
-                parentPath = parentPath + s + "/";
-            }
-
-            parentPath = parentPath.Remove(parentPath.Length - 1);
-
-            Files.HasPlugin(reference);
-            
-            Array result = Files.GetFilesFromDir(parentPath, true, Files.FilterModuleMeta);
-    
-            _resource = GD.Load((string)result[0]);
-
-            _name = (string) _resource.Get("name");
-            _desc = (string) _resource.Get("desc");
-            _version = (string) _resource.Get("version");
-            _deps = (Array) _resource.Get("deps");
-            _depsVersions = (Array) _resource.Get("deps_vers");
-        }
-    }
-
-    public class WayfarerProjectSettings
-    {
-        private Resource _resource;
-        public Resource Resource => _resource;
-
-        private Array _installedModules;
-        private Dictionary _settings;
-
-        public WayfarerProjectSettings()
-        {
-            _resource = GD.Load("res://wfsettings.tres");
-
-            _installedModules = (Array) _resource.Get("installed_modules");
-            _settings = (Dictionary) _resource.Get("settings");
-        }
-        
-        public void Add(string settingPath, object value, string desc = "")
-        {
-            if (_settings.ContainsKey(settingPath))
-            {
-                Dictionary presentValue = (Dictionary)_settings[settingPath];
-
-                if (presentValue != null && presentValue["value"] != value)
-                {
-                    presentValue["value"] = value;
-                }
-
-                if (presentValue != null && (string) presentValue["desc"] != desc)
-                {
-                    if (!string.IsNullOrEmpty(desc))
-                    {
-                        presentValue["desc"] = desc;
-                    }
-                }
-            }
-            else
-            {
-                Dictionary settingValues = new Dictionary {{"value", value}, {"desc", desc}};
-
-                _settings.Add(settingPath, settingValues);
-            }
-        }
-
-        public void Update(string settingPath, object value)
-        {
-            // we might want to make a more streamlined version of AddSetting for updating purposes - for now we'll just wrap it
-            Add(settingPath, value);
-        }
-
-        public bool Contains(string settingPath)
-        {
-            return _settings.ContainsKey(settingPath);
         }
     }
 }
