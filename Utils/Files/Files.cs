de82f8efac4de8dc6d33d33f8c44747efdb58b9c
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-07 12:54:03 +0300

    Expanded the Files behaviour, mostly with sanity checks and exception handling&throwing

--- Utils/Files/Files.cs
+++ Utils/Files/Files.cs
@@ -1,4 +1,7 @@
-﻿using Godot;
+﻿using System;
+using Godot;
+using Wayfarer.Utils.Debug;
+using Wayfarer.Utils.Debug.Exceptions;
 using Array = Godot.Collections.Array;
 using Object = Godot.Object;
 
@@ -17,14 +20,23 @@ namespace Wayfarer.Utils.Files
         
         public static Object GdFileUtils => GetGdFileUtils();
 
-        public static Array GetFilesFromDir(string dirPath, bool returnAsPath = true, Array filter = null)
+        public static Array GetFilesFromDir(string dirPath, bool returnAsPath = true, Array filter = null, bool expectEmpties = true)
         {
             if (filter == null)
             {
                 filter = FilterDefault;
             }
 
-            Array result = (Array) GdFileUtils.Call("get_files_from_dir", dirPath, returnAsPath, filter);
+            Array result = (Array) GdFileUtils.Call("get_files_from_dir", dirPath, returnAsPath, filter, expectEmpties);
+
+            if (result == null)
+            {
+                throw new GdCallReturnNullException("GdCall \"" + "file_utils.get_giles_from_dir()" + "\" returned null");
+            }
+            if (result.Count < 1)
+            {
+                Log.Wf.Editor("GdCall \"" + "file_utils.get_giles_from_dir()" + "\" returned an empty array", true);
+            }
         
             return result;
         }
@@ -51,14 +63,30 @@ namespace Wayfarer.Utils.Files
         public static void SetPlugin(EditorPlugin plugin)
         {
             GdFileUtils.Call("set_plugin", plugin);
+
+            CheckIfPluginWasSetCorrectly(plugin);
+        }
+
+        private static void CheckIfPluginWasSetCorrectly(EditorPlugin plugin)
+        {
+            if (!HasPlugin())
+            {
+                throw new GdCallFailedException("(Files static) After calling SetPlugin() the plugin (" + plugin.Name + ") was still not set");
+            }
         }
         
         private static Object GetGdFileUtils()
         {
-            if (_gdFileUtils != null) return _gdFileUtils;
+            if (_gdFileUtils != null && Object.IsInstanceValid(_gdFileUtils)) return _gdFileUtils;
             
             GDScript script = GD.Load<GDScript>("res://Addons/Wayfarer/file_utils.gd");
             _gdFileUtils = script.New();
+
+            if (_gdFileUtils == null)
+            {
+                throw new NullReferenceException();
+            }
+            
             return _gdFileUtils;
         }
     }
