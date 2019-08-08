e3afae9a8eccdca756ed82b9429c50c8b546a419
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-08 15:12:07 +0300

    Added in instantiate and dispose wrappers for overwatch's use

--- GDInterfaces/helpers.gd
+++ GDInterfaces/helpers.gd
@@ -7,3 +7,19 @@ static func get_children_recursive(var node : Node) -> Array:
 	var children = cs_instance.call("GetChildrenRecursive", node);
 	cs_instance.queue_free();
 	return children;
+	
+static func instantiate_statics() -> void:
+	var cs_bridge = load("res://Addons/Wayfarer/Utils/Gd.cs");
+	var cs_instance = cs_bridge.new();
+	cs_instance.call("InitializeStatics");
+	cs_instance.queue_free();
+	print(" --- Initialized statics!");
+	pass
+	
+static func dispose_statics() -> void:
+	var cs_bridge = load("res://Addons/Wayfarer/Utils/Gd.cs");
+	var cs_instance = cs_bridge.new();
+	cs_instance.call("DisposeStatics");
+	cs_instance.queue_free();
+	print(" --- Disposed statics!");
+	pass
