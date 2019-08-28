9f7782e57e5cd35905fbe8f4f8036946a8cc2e76
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-28 12:12:55 +0300

    Added in RadialGradient helper to create a 2d array of colors to represent the gradient

---
+++ Utils/Helpers/Gradients.cs
@@ -0,0 +1,28 @@
+﻿using Godot;
+
+namespace Wayfarer.Utils.Helpers
+{
+    public class Gradients
+    {
+        public static Color[,] RadialGradient(int width, int height, Gradient gradient)
+        {
+            Color[,] data = new Color[width, height];
+            Vector2 size = new Vector2(width, height);
+            Vector2 radius = (size - new Vector2(1, 1)) / 2;
+            float ratio = size.x / size.y;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    float dist = new Vector2(x / ratio, y).DistanceTo(new Vector2(radius.x / ratio, radius.y));
+                    float offset = dist / radius.y;
+                    Color color = gradient.Interpolate(offset);
+                    data[x, y] = color;
+                }
+            }
+
+            return data;
+        }
+    }
+}
