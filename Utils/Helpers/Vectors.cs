0d4f46a4df812f155592c9580603d87810e35cbb
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-28 12:14:16 +0300

    Added in Vector3Int for shorthand usage in cases like grids, where the values are always full integers

--- Utils/Helpers/Vectors.cs
+++ Utils/Helpers/Vectors.cs
@@ -7,4 +7,22 @@ namespace Wayfarer.Utils.Helpers
         public static Vector2 Zero2 = new Vector2(0, 0);
         public static Vector3 Zero3 = new Vector3(0, 0, 0);
     }
+
+    public struct Vector3Int
+    {
+        private int _x;
+        private int _y;
+        private int _z;
+
+        public int x => _x;
+        public int y => _y;
+        public int z => _z;
+
+        public Vector3Int(int x, int y, int z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+    }
 }
