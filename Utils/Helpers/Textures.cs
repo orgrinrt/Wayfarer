1cc85b638dc25268b197e5e4b1fd44d7e8311d7f
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-28 12:13:22 +0300

    Added in a shorthand method to generate a ImageTexture from a 2d color array representing a radial gradient

--- Utils/Helpers/Textures.cs
+++ Utils/Helpers/Textures.cs
@@ -8,5 +8,32 @@ namespace Wayfarer.Utils.Helpers
         {
             return new Vector2(sourceSize.x / (sourceSize.x / targetWidth) / 250, (sourceSize.y / (sourceSize.y / targetHeight)) / 250);
         }
+
+        public static ImageTexture RadialGradient(int width, int height, Gradient gradient)
+        {
+            Image image = new Image();
+            
+            image.Create(width, height, true, Image.Format.Rgb8);
+            image.Lock();
+
+            Color[,] colors = Gradients.RadialGradient(width, height, gradient);
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    image.SetPixel(x, y, colors[x, y]);
+                }
+            }
+            
+            image.Unlock();
+            
+            ImageTexture tex = new ImageTexture();
+            tex.CreateFromImage(image);
+
+            return tex;
+        }
+
+        
     }
 }
