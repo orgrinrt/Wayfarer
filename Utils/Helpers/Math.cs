a1509efd374d6b9d1d36540a213fe676dc130ccb
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-28 12:13:48 +0300

    Added in various math helper methods

--- Utils/Helpers/Math.cs
+++ Utils/Helpers/Math.cs
@@ -41,5 +41,45 @@ namespace Wayfarer.Utils.Helpers
         {
             return (((input - min) * 100) / (max - min)) / 100;
         }
+        
+        public static double Clamp(double v, double l, double h)
+        {
+            if (v < l) v = l;
+            if (v > h) v = h;
+            return v;
+        }
+
+        public static double Lerp(double t, double a, double b)
+        {
+            return a + t * (b - a);
+        }
+
+        public static double QuinticBlend(double t)
+        {
+            return t * t * t * (t * (t * 6 - 15) + 10);
+        }
+
+        public static double Bias(float b, float t)
+        {
+            return Mathf.Pow(t, Mathf.Log(b) / Mathf.Log(0.5f));
+        }
+
+        public static double Gain(float g, float t)
+        {
+            if (t < 0.5)
+            {
+                return Bias(1.0f - g, 2.0f * t) / 2.0;
+            }
+            else
+            {
+                return 1.0 - Bias(1.0f - g, 2.0f - 2.0f * t) / 2.0;
+            }
+        }
+
+        public static int Mod(int x, int m)
+        {
+            int r = x % m;
+            return r < 0 ? r + m : r;
+        }    
     }
 }
