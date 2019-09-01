4ab6ac21a0ad2a05b760d33e960bd987367db8c2
orgrinrt <ollipekka.toimela@gmail.com>
2019-09-01 13:42:02 +0300

    Added in a ThreadedProcess abstract class, this is a very crude and needs to be expanded further to make it more usable, but for now it seems to be enough

---
+++ Utils/Threading/ThreadedProcess.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Wayfarer.Utils.Threading
+{
+    public abstract class ThreadedProcess : IDisposable
+    {
+        private bool _isDone = false;
+        private object _handle = new object();
+        private Thread _thread;
+
+        public bool IsDone => CheckIfDone();
+
+        public virtual void Start()
+        {
+            _thread = new Thread(Run);
+            _thread.Start();
+        }
+
+        public virtual void Abort()
+        {
+            _thread.Abort();
+        }
+
+        public virtual bool Update()
+        {
+            if (CheckIfDone())
+            {
+                OnFinished();
+                return true;
+            }
+            return false;
+        }
+        
+        private void Run()
+        {
+            Logic();
+            
+            _isDone = true;
+        }
+
+        protected abstract void Logic();
+
+        protected virtual void OnFinished()
+        {
+            
+        }
+
+        private bool CheckIfDone()
+        {
+            bool isDone = false;
+            
+            lock (_handle)
+            {
+                isDone = _isDone;
+            }
+
+            return isDone;
+        }
+
+        protected void Done()
+        {
+            _isDone = true;
+        }
+
+        public virtual void Dispose()
+        {
+            Abort();
+            _handle = null;
+            _thread = null;
+        }
+    }
+}
